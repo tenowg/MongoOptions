3247135df8e2d15485aae9ac593432ef1f956775
Craig Russell <tenowg@gmail.com>
2026-02-01 00:44:21 -0500

    Added some base extensions that can be use either with the current project and the new blazor project

---
+++ Extensions/PropertyExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace MongoOptions.Extensions
+{
+    public static class PropertyExtensions
+    {
+        extension(PropertyInfo prop)
+        {
+            public Expression<Func<TProp>> CreateLambda<TProp>(object SettingsObject)
+            {
+                var constant = Expression.Constant(SettingsObject);
+                var property = Expression.Property(constant, prop.Name);
+
+                Expression finalExpression = property;
+                if (prop.PropertyType != typeof(TProp))
+                {
+                    finalExpression = Expression.Call(property, typeof(object).GetMethod("ToString")!);
+                }
+
+                return Expression.Lambda<Func<TProp>>(finalExpression);
+            }
+
+            public Expression<Func<object>> CreateLambda(object SettingsObject, Type type)
+            {
+                var constant = Expression.Constant(SettingsObject);
+                var property = Expression.Property(constant, prop.Name);
+
+                Expression finalExpression = property;
+                if (prop.PropertyType != type)
+                {
+                    finalExpression = Expression.Call(property, typeof(object).GetMethod("ToString")!);
+                }
+
+                return Expression.Lambda<Func<object>>(finalExpression);
+            }
+
+            public string GetDisplayName() =>
+                prop.GetCustomAttribute<DisplayAttribute>()?.Name ?? prop.Name;
+        }
+    }
+}
---
+++ Extensions/TypeExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace MongoOptions.Extensions
+{
+    public static class TypeExtensions
+    {
+        extension(Type type)
+        {
+            public bool IsNumber()
+            {
+                type = Nullable.GetUnderlyingType(type) ?? type;
+                return type == typeof(int) || type == typeof(long) ||
+                       type == typeof(double) || type == typeof(decimal);
+            }
+
+            public bool IsSimpleType()
+            {
+                return type.IsPrimitive ||
+                       type == typeof(string) ||
+                       type == typeof(decimal) ||
+                       type == typeof(DateTime) ||
+                       type == typeof(Guid);
+            }
+
+            public Type GetUnderlyingType()
+                => Nullable.GetUnderlyingType(type) ?? type;
+
+            public IEnumerable<PropertyInfo> GetEligibleProperties() =>
+                type.GetProperties().Where(p => p.CanWrite && p.CanRead);
+
+            public object? GetDefaultValue()
+            {
+                // 1. Strings are the special case
+                if (type == typeof(string)) return string.Empty;
+
+                // 2. Guids need a valid non-null value to be useful
+                if (type == typeof(Guid)) return Guid.NewGuid();
+
+                // 3. For everything else (int, bool, etc.), Activator or default works
+                if (type.IsValueType)
+                {
+                    return Activator.CreateInstance(type);
+                }
+
+                return null;
+            }
+        }
+    }
+}
